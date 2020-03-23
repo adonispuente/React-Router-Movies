08d74ac858b3d072c2d34582686ad1724ef688ba
Adonis Puente <adonispuente@Adoniss-Air.attlocal.net>
2020-03-23 17:19:26 -0400

    done, no stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,46 @@
-import React, { useState } from 'react';
-
-import SavedList from './Movies/SavedList';
+import React, { useState } from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import SavedList from "./Movies/SavedList";
+import Movie from "./Movies/Movie";
+import MovieList from "./Movies/MovieList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <nav>
+        <div className="nav-links">
+          {/* <Link to="/">Home</Link> */}
+          {/* <Link to="/movies/:id"> Individual Movie</Link> */}
+        </div>
+      </nav>
+
+      <Switch>
+        <Route path="/movies/:id">
+          <Movie />
+        </Route>
+        <Route exact path="/">
+          <MovieList />
+        </Route>
+      </Switch>
     </div>
   );
 };
 
 export default App;
+
+/* <SavedList list={savedList} />
+<Route path="/" component={MovieList}>
+  <Item items={MovieList} />
+</Route>
+
+<Route path="/movies/:itemID" component={Movie}>
+  <Item items={Movie} />
+</Route>
+</div> */
